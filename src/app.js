eacb34426aff8eb98f3f25ac2475bde1330bf2d6
Valentyna Baranova <valentynabaranova5@gmail.com>
2022-07-18 17:47:25 +0300

    Added forecast array

--- src/app.js
+++ src/app.js
@@ -22,6 +22,32 @@ if (min <= 9) {
 let dayTime = document.querySelector("li#date");
 dayTime.innerHTML = `${day}, ${hour}:${min} `;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+       <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="http://openweathermap.org/img/wn/50d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max"> 18° </span>
+        <span class="weather-forecast-temperature-min"> 12° </span>
+      </div>
+    </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function searchCity(city) {
   var apiKey = "03cc9b8479bb5a2db9ec53fe58f6ab8a";
   var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -77,6 +103,7 @@ function Getcelsius(event) {
 }
 
 let celsiusTemperature = null;
+displayForecast();
 
 let fahrenheitType = document.querySelector("a#fahrenheit-link");
 fahrenheitType.addEventListener("click", Getfahrenheit);
